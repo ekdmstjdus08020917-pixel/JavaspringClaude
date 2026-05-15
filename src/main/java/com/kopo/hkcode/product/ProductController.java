541b27c4caade30ce1cd3a85fb20b9445c419f3e
ekdmstjdus <init.defaultBrach>
2026-05-15 18:28:06 +0900

    feat: productgroup 실제 입력값 반영 후 마무리

--- src/main/java/com/kopo/hkcode/product/ProductController.java
+++ src/main/java/com/kopo/hkcode/product/ProductController.java
@@ -2,8 +2,6 @@ package com.kopo.hkcode.product;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,15 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 
-    @Autowired
     private ProductService productService;
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @PostMapping("/getMaxQty")
-    public Map<String, Object> getMaxQty(@RequestParam("regionid") String regionid,
-                                          @RequestParam("productgroup") String productgroup) {
+    public Map<String, Object> getMaxQty(
+            @RequestParam("regionid") String regionid,
+            @RequestParam("productgroup") String productgroup) {
 
         String result = productService.getMaxQtyByRegion(regionid, productgroup);
-
         Map<String, Object> response = new HashMap<>();
         response.put("regionId", regionid);
         response.put("productGroup", productgroup);
